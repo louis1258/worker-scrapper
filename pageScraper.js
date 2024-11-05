3108ce6d85b46bd99d923530942aca96f7deb0ed
nghiahothanh <nghia.hothanh.319@gmail.com>
2024-11-05 23:00:00 +0700

    up

--- pageScraper.js
+++ pageScraper.js
@@ -17,10 +17,10 @@ user_agent_list = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
 ]
 const userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36";
-const proxyUrl = 'hndc21.proxyxoay.net:32051';  // Replace with your actual proxy URL and port
+const proxyUrl = 'hndc31.proxyxoay.net:15925';  // Replace with your actual proxy URL and port
 const proxyUsername = 'louis1258';
 const proxyPassword = 'Htn@1258';
-const apiKey = '6f7badf1-a51f-4f47-9b1b-daaf0f713b70';
+const apiKey = '6c1be37c-bbcf-425f-ad20-3c3d4ef2f7bd';
 const userAgentList = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
@@ -275,10 +275,18 @@ const scraperObject = {
                     );
 
                     // Save the uploaded image URLs to the chapter data
+                    const numberMatch = chapter.title.match(/\d+(\.\d+)?$/);
+                    let chapterNumber = 0
+                    if (numberMatch) {
+                         chapterNumber = parseFloat(numberMatch[0]); 
+                    } else {
+                        chapterNumber = chapter.title
+                    }
+                    console.log(chapterNumber,'chapterNumber');
                     const chapterData = {
                         comic: `${resultComic._id}`,
                         order: index+1,
-                        title: `Chapter ${index+1}`,
+                        title: `Chapter ${numberMatch}`,
                         images: uploadedImageUrls.filter(url => url) // Filter out any null results
                     };
 
