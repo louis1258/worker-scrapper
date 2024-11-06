8b1b27e7a3807c1c30012f6b2410307046285478
nghiahothanh <nghia.hothanh.319@gmail.com>
2024-11-07 00:55:03 +0700

    version2

--- pageScraper.js
+++ pageScraper.js
@@ -11,11 +11,6 @@ const randomDelay = async (min = 500, max = 1500) => {
     const delay = Math.floor(Math.random() * (max - min + 1)) + min;
     return new Promise((resolve) => setTimeout(resolve, delay));
 };
-user_agent_list = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
-]
 const userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36";
 const proxyUrl = 'hndc31.proxyxoay.net:15925';  // Replace with your actual proxy URL and port
 const proxyUsername = 'louis1258';
@@ -87,7 +82,7 @@ const scraperObject = {
 
 
         // Loop through each of those links, open a new page instance, and get the relevant data
-        let pagePromise = async (link) => {
+        let pagePromise = async (payload) => {
             const newPage = await browser.newPage();
             // Set API key in request headers
             await newPage.setExtraHTTPHeaders({
@@ -105,202 +100,68 @@ const scraperObject = {
             });
 
             try {
-                // await newPage.goto(link, { waitUntil: 'domcontentloaded', timeout: 20000 });
-                await gotoWithRetry(newPage, link, 3);
-                await randomDelay(100, 500);
-                dataObj['title'] = await newPage.$eval('.book_detail > .book_info > .book_other > h1', title => title.textContent);
-                const coverImageSrc = await newPage.$eval(
-                    '.book_detail > .book_info > .book_avatar > img',
-                    img => img ? img.src : null
-                );
+                    await gotoWithRetry(newPage, payload.chapter, 3);
 
-                console.log('Cover Image Source:', coverImageSrc); // Log the image source
-
-                if (!coverImageSrc) {
-                    console.error('Cover image not found');
-                    return null;
-                }
-
-                try {
-
-
-                    const arrayBuffer = await fetchWithRetry(coverImageSrc); // Get the image as ArrayBuffer
-                    const buffer = Buffer.from(arrayBuffer); // Convert ArrayBuffer to Buffer
-                    const base64String = buffer.toString('base64'); // Convert buffer to base64
-                    const ext = coverImageSrc.split('.').pop().split('?')[0]; // Extract the extension from the URL
-
-
-                    // Construct the full base64 string
-                    const fullBase64String = `data:image/${ext};base64,${base64String}`;
-
-                    // Check the format of the full base64 string
-                    const match = fullBase64String.match(/^data:image\/(png|jpeg|jpg);base64,(.+)$/);
-                    if (match) {
-                        const imgExt = match[1];  // Image format
-                        const data = match[2]; // Base64 data
-                        const finalBuffer = Buffer.from(data, 'base64'); // Convert base64 data to buffer
-
-                        // Create the file path
-                        const filePath = path.join(__dirname, `${dataObj['title']}.${imgExt}`);
-                        // Save the image to the filesystem
-                        fs.writeFileSync(filePath, finalBuffer);
-
-                        // Upload the image after saving
-                        const url = await upload(`${dataObj['title']}.${imgExt}`); // Ensure you use the correct file path for upload
-                        dataObj['coverImage'] = url;
-                    } else {
-                        console.error('Base64 string format is incorrect after construction.');
-                    }
-                } catch (error) {
-                    console.error(`Error fetching or processing the image from ${coverImageSrc}:`, error);
-                }
-                dataObj['genres'] = await newPage.$$eval('.book_detail > .book_info > .book_other > .list01 .li03 > a', genres => {
-                    return genres.map(genre => genre.textContent.trim() ?? null);  // Extract and trim the text content of each genre
+                // Use $eval to extract image sources for the chapter
+                const imagesSrc = await newPage.$$eval('.page-chapter > img.lazy', images => {
+                    return images.map(img => img.src); // Extract 'src' attributes
                 });
 
-                const dataComicType = {
-                    name: 'Truyện tranh',
-                    description: "Thể loại có nội dung trong sáng và cảm động, thường có các tình tiết gây cười, các xung đột nhẹ nhàng",
-                    status: "Active",
-                }
-                const typeComicArray = await Promise.all(
-                    dataObj['genres'].map(async (type) => {
-                        dataComicType.name = type;
-                        const typeComic = await createComicType(dataComicType);
-                        return typeComic._id; // Trả về _id của ComicType
+                // Process each image source
+                const uploadedImageUrls = await Promise.all(
+                    imagesSrc.map(async (src, imageIndex) => {
+                        if (src) {
+                            try {
+                                const arrayBuffer = await fetchWithRetry(src); // Get the image as ArrayBuffer
+                                const buffer = Buffer.from(arrayBuffer); // Convert ArrayBuffer to Buffer
+                                const base64String = buffer.toString('base64'); // Convert buffer to base64
+                                const ext = src.split('.').pop().split('?')[0]; // Extract the extension from the URL
+
+
+                                // Construct the full base64 string
+                                const fullBase64String = `data:image/${ext};base64,${base64String}`;
+
+                                // Check the format of the full base64 string
+                                const match = fullBase64String.match(/^data:image\/(png|jpeg|jpg);base64,(.+)$/);
+                                if (match) {
+                                    const imgExt = match[1];  // Image format
+                                    const data = match[2]; // Base64 data
+                                    const finalBuffer = Buffer.from(data, 'base64'); // Convert base64 data to buffer
+
+                                    // Create the file path for each chapter image
+                                    const filePath = path.join(__dirname, `chapter_image_${payload.order}_${payload.comic}${imageIndex}.${imgExt}`);
+                                    // Save the image to the filesystem
+                                    fs.writeFileSync(filePath, finalBuffer);
+                                    console.log('Image saved at:', filePath); // Log file save confirmation
+
+                                    // Upload the image after saving
+                                    const url = await upload(`chapter_image_${payload.order}_${payload.comic}${imageIndex}.${imgExt}`); // Upload the image
+                                    return url; // Return the uploaded image URL
+                                } else {
+                                    console.error('Base64 string format is incorrect after construction.');
+                                }
+                            } catch (error) {
+                                console.error(`Error fetching or processing the image from ${src}:`, error);
+                                return null; // Return null if there's an error
+                            }
+                        }
+                        return null; // Return null if there's no valid src
                     })
                 );
 
-                dataObj['genres'] = typeComicArray
-                const check = await waitForElement(newPage, '.book_detail > .story-detail-info.detail-content');
-                dataObj['description'] = await newPage.evaluate(() => {
-                    const checkInner = document.querySelector('body > div.content > div.div_middle > div.main_content > div.book_detail > div.story-detail-info.detail-content.readmore-js-section.readmore-js-collapsed > p');
-                    if (checkInner)
-                        return checkInner.textContent.trim();
-                    else {
-                        const checkOuter = document.querySelector('body > div.content > div.div_middle > div.main_content > div.book_detail > div.story-detail-info.detail-content > p')
-                        if (checkOuter) {
-                            return checkOuter.textContent.trim();
-                        }
-                        const checkElement = document.querySelector('body > div.content > div.div_middle > div.main_content > div.book_detail > div.story-detail-info.detail-content')
-                        if (checkElement) {
-                            return checkElement.textContent.trim() ?? null;
-                        }
-                        else {
-                            const checkContinue = document.querySelector(' body > div.content > div.div_middle > div.main_content > div.book_detail > div.story-detail-info.detail-content')
-                            return checkContinue ? checkContinue.textContent.trim() : null;
-                        }
-                    }
-                });
-                console.log(dataObj['description']);
-                // Scrape chapters
-                dataObj.slug = createSlug(dataObj.title)
-                const resultComic = await createComic(dataObj);
-                console.log('Comic created:', resultComic);
+                const chapterData = {
+                    comic: `${payload.comic}`,
+                    order: `${payload.order}`,
+                    title: `Chapter ${payload.order}`,
+                    images: uploadedImageUrls.filter(url => url) // Filter out any null results
+                };
 
-
-                dataObj['chapter'] = await newPage.$$eval(
-                    'body > div.content > div.div_middle > div.main_content > div.book_detail > div.list_chapter > div > .works-chapter-item',
-                    chapters => {
-                        return chapters.map(chapter => {
-                            const linkElement = chapter.querySelector('a');
-                            return {
-                                title: linkElement.textContent.trim(),
-                                link: linkElement.href
-                            };
-                        });
-                    }
-                );
-
-                // Fetch chapter content
-                for (const [index, chapter] of dataObj['chapter'].reverse().entries()) {
-                    console.log(chapter);
-                    try {
-                        await randomDelay(100, 500);
-                        // await newPage.goto(chapter.link, { waitUntil: 'domcontentloaded', timeout: 200000 });
-                        await gotoWithRetry(newPage, chapter.link, 3);
-
-                    } catch (error) {
-                        console.error(`Failed to load ${chapter.link}:`, error);
-                        continue; // Skip to the next chapter
-                    }
-
-                    // Use $eval to extract image sources for the chapter
-                    const imagesSrc = await newPage.$$eval('.page-chapter > img.lazy', images => {
-                        return images.map(img => img.src); // Extract 'src' attributes
-                    });
-
-                    // Process each image source
-                    const uploadedImageUrls = await Promise.all(
-                        imagesSrc.map(async (src, imageIndex) => {
-                            if (src) {
-                                try {
-
-
-                                    const arrayBuffer = await fetchWithRetry(src); // Get the image as ArrayBuffer
-                                    const buffer = Buffer.from(arrayBuffer); // Convert ArrayBuffer to Buffer
-                                    const base64String = buffer.toString('base64'); // Convert buffer to base64
-                                    const ext = src.split('.').pop().split('?')[0]; // Extract the extension from the URL
-
-
-                                    // Construct the full base64 string
-                                    const fullBase64String = `data:image/${ext};base64,${base64String}`;
-
-                                    // Check the format of the full base64 string
-                                    const match = fullBase64String.match(/^data:image\/(png|jpeg|jpg);base64,(.+)$/);
-                                    if (match) {
-                                        const imgExt = match[1];  // Image format
-                                        const data = match[2]; // Base64 data
-                                        const finalBuffer = Buffer.from(data, 'base64'); // Convert base64 data to buffer
-
-                                        // Create the file path for each chapter image
-                                        const filePath = path.join(__dirname, `chapter_image_${index}_${resultComic._id}${imageIndex}.${imgExt}`);
-                                        // Save the image to the filesystem
-                                        fs.writeFileSync(filePath, finalBuffer);
-                                        console.log('Image saved at:', filePath); // Log file save confirmation
-
-                                        // Upload the image after saving
-                                        const url = await upload(`chapter_image_${index}_${resultComic._id}${imageIndex}.${imgExt}`); // Upload the image
-                                        return url; // Return the uploaded image URL
-                                    } else {
-                                        console.error('Base64 string format is incorrect after construction.');
-                                    }
-                                } catch (error) {
-                                    console.error(`Error fetching or processing the image from ${src}:`, error);
-                                    return null; // Return null if there's an error
-                                }
-                            }
-                            return null; // Return null if there's no valid src
-                        })
-                    );
-
-                    // Save the uploaded image URLs to the chapter data
-                    const parts = chapter.title.split(" ");
-                    let chapterNumber = null;
-
-                    for (const part of parts) {
-                        const number = parseFloat(part);
-                        if (!isNaN(number)) {
-                            chapterNumber = number;
-                            break; 
-                        }
-                    }
-                    const chapterData = {
-                        comic: `${resultComic._id}`,
-                        order: index + 1,
-                        title: `Chapter ${chapterNumber}`,
-                        images: uploadedImageUrls.filter(url => url) // Filter out any null results
-                    };
-
-                    // Save chapter data to your database or perform any action with it
-                    const chapterCreate = await createChapter(chapterData);
-                }
-
-
-
-                return dataObj;
+                // Save chapter data to your database or perform any action with it
+                const chapterCreate = await createChapter(chapterData);
+                console.log(chapterCreate);
+                return chapterCreate;
             } catch (err) {
-                console.error(`Error scraping link: ${link}`, err);
+                console.error(`Error scraping link: `, err);
                 await newPage.close();
             }
             finally {
@@ -327,9 +188,9 @@ const scraperObject = {
 
                 try {
                     // Process the task here
-                    const currentPageData = await pagePromise(task.href);
+                    const currentPageData = await pagePromise(task);
                     scrapedData.push(currentPageData);
-                    console.log(`✅ Task completed: ${task.href}`);
+                    console.log(`✅ Task completed: ${task.chapter}`);
                     channel.ack(message); // Acknowledge the message after successful processing
                 } catch (error) {
                     console.error(`❌ Error processing task: ${task.href}`, error);
